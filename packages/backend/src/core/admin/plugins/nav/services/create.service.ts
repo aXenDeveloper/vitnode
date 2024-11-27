193a49ddd2d5a33cd0c7ce7b9f5ee93faa410f89
aXenDeveloper <axendeveloper@gmail.com>
2024-11-27 20:50:25 +0100

    chore: clean up

--- packages/backend/src/core/admin/plugins/nav/services/create.service.ts
+++ packages/backend/src/core/admin/plugins/nav/services/create.service.ts
@@ -6,8 +6,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { existsSync } from 'fs';
-import { readdir, readFile, writeFile } from 'fs/promises';
-import { join } from 'path';
+import { readFile, writeFile } from 'fs/promises';
 import { ParentNavAuthAdminObj } from 'vitnode-shared/admin/auth.dto';
 import { ConfigPlugin } from 'vitnode-shared/admin/plugin.dto';
 import { CreateNavPluginsAdminBody } from 'vitnode-shared/admin/plugins/nav.dto';
@@ -62,30 +61,6 @@ export class CreateNavPluginsAdminService extends HelpersAdminNavPluginsService 
         keywords,
       });
     }
-
-    const langPathFolder = ABSOLUTE_PATHS.plugin({
-      code: plugin_code,
-    }).frontend.languages;
-    // Get all files
-    const langs = await readdir(langPathFolder);
-    await Promise.all(
-      langs.map(async lang => {
-        const langFilePath = join(langPathFolder, lang);
-        if (!langFilePath.endsWith('.json')) return;
-
-        const langFile = JSON.parse(await readFile(langFilePath, 'utf8'));
-        const langCode = parent_code ? `${parent_code}_${code}` : code;
-        langFile[`admin_${plugin_code}`] = {
-          ...langFile[`admin_${plugin_code}`],
-          nav: {
-            ...langFile[`admin_${plugin_code}`].nav,
-            [langCode]: `admin_${plugin_code}.nav.${langCode}`,
-          },
-        };
-
-        await writeFile(langFilePath, JSON.stringify(langFile, null, 2));
-      }),
-    );
 
     await writeFile(pathConfig, JSON.stringify(config, null, 2));
 
