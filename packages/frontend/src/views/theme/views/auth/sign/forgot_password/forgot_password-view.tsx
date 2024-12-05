dc402416980d7de4a6b5987701511033cc6a76a9
aXenDeveloper <axendeveloper@gmail.com>
2024-12-05 14:36:42 +0100

    fix(backend): Import files from packages

--- packages/frontend/src/views/theme/views/auth/sign/forgot_password/forgot_password-view.tsx
+++ packages/frontend/src/views/theme/views/auth/sign/forgot_password/forgot_password-view.tsx
@@ -1,15 +1,20 @@
+import { getMiddlewareData } from '@/api/get-middleware-data';
 import { TranslationsProvider } from '@/components/translations-provider';
+import { notFound } from 'next/navigation';
 
 import { FormForgotPassword } from './form';
 import { ResetPassword } from './reset/reset-password';
 
-export const ForgotPasswordView = ({
+export const ForgotPasswordView = async ({
   userId,
   token,
 }: {
   token: string | undefined;
   userId: string | undefined;
 }) => {
+  const { is_email_enabled } = await getMiddlewareData();
+  if (!is_email_enabled) notFound();
+
   if (userId && token) {
     return (
       <TranslationsProvider
