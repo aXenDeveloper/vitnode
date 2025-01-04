cea54a1639ff37eeaa08066d99e16cb8f7e0aed4
aXenDeveloper <axendeveloper@gmail.com>
2025-01-04 12:17:50 +0100

    fix(cli): Update default page when is more then one plugins

--- packages/backend/scripts/update-plugins.ts
+++ packages/backend/scripts/update-plugins.ts
@@ -30,60 +30,58 @@ export const updatePlugins = async ({
       },
     });
 
-    await Promise.all(
-      plugins.map(async (code, index) => {
-        const pluginPath = join(pluginsPath, code);
-        const configPath = join(pluginPath, 'config.json');
-        if (!existsSync(configPath)) {
-          return;
-        }
+    for (const [index, code] of plugins.entries()) {
+      const pluginPath = join(pluginsPath, code);
+      const configPath = join(pluginPath, 'config.json');
+      if (!existsSync(configPath)) {
+        continue;
+      }
 
-        const config = JSON.parse(
-          await readFile(join(pluginPath, 'config.json'), 'utf8'),
-        );
+      const config = JSON.parse(
+        await readFile(join(pluginPath, 'config.json'), 'utf8'),
+      );
 
-        if (config.allow_default) {
-          isDefaultIndex = index;
-        }
+      if (config.allow_default && isDefaultIndex === null) {
+        isDefaultIndex = index;
+      }
 
-        const plugin = pluginsFromDatabase.find(
-          plugin => plugin.code === config.code,
-        );
+      const plugin = pluginsFromDatabase.find(
+        plugin => plugin.code === config.code,
+      );
 
-        if (plugin) {
-          await tx
-            .update(core_plugins)
-            .set({
-              name: config.name,
-              description: config.description,
-              support_url: config.support_url,
-              author: config.author,
-              author_url: config.author_url,
-              allow_default: config.allow_default,
-              version: config.version,
-              version_code: config.version_code,
-            })
-            .where(eq(core_plugins.id, plugin.id));
-        } else {
-          await tx.insert(core_plugins).values([
-            {
-              name: config.name,
-              description: config.description,
-              code: config.code,
-              support_url: config.support_url,
-              author: config.author,
-              author_url: config.author_url,
-              allow_default: config.allow_default,
-              version: config.version,
-              version_code: config.version_code,
-              default: isDefaultIndex === index && !defaultPlugin,
-            },
-          ]);
-        }
+      if (plugin) {
+        await tx
+          .update(core_plugins)
+          .set({
+            name: config.name,
+            description: config.description,
+            support_url: config.support_url,
+            author: config.author,
+            author_url: config.author_url,
+            allow_default: config.allow_default,
+            version: config.version,
+            version_code: config.version_code,
+          })
+          .where(eq(core_plugins.id, plugin.id));
+      } else {
+        await tx.insert(core_plugins).values([
+          {
+            name: config.name,
+            description: config.description,
+            code: config.code,
+            support_url: config.support_url,
+            author: config.author,
+            author_url: config.author_url,
+            allow_default: config.allow_default,
+            version: config.version,
+            version_code: config.version_code,
+            default: isDefaultIndex === index && !defaultPlugin,
+          },
+        ]);
+      }
 
-        await tx.execute(sql`commit`);
-      }),
-    );
+      await tx.execute(sql`commit`);
+    }
 
     // Remove plugins that are not in the plugins folder
     const pluginsToDelete = pluginsFromDatabase.filter(
