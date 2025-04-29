a69ff33ff8cdd3961848b326515a3fc4a4fda21c
aXenDeveloper <axendeveloper@gmail.com>
2025-04-29 21:46:44 +0200

    feat: Add test plan

---
+++ packages/vitnode/src/tests/setup.ts
@@ -0,0 +1,10 @@
+import '@testing-library/react';
+// import { expect, vi } from 'vitest';
+
+// Set up global mocks if needed
+// vi.stubGlobal('fetch', vi.fn());
+
+// Custom matchers can be added here if needed
+// expect.extend({ ... });
+
+// Add global environment setup
---
+++ packages/vitnode/vitest.config.ts
@@ -0,0 +1,33 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/build/**',
+        '**/.next/**',
+        '**/.turbo/**',
+        '**/coverage/**',
+        '**/src/tests/**', // Exclude test setup files
+        '**/src/emails/**', // Exclude email templates
+        '**/config/**', // Exclude config files
+        '**/scripts/**', // Exclude scripts
+        '**/*.config.*', // Exclude config files (e.g., vitest.config.ts)
+        '**/*.d.ts', // Exclude type definition files
+      ],
+    },
+    alias: {
+      '@': './src',
+    },
+  },
+});
