bcf2a8e3d1bf1b96ae760d500d5755bdc2a77810
aXenDeveloper <axendeveloper@gmail.com>
2025-05-25 18:05:47 +0200

    refactor(check-plugin-name): ✨ Update `checkPluginName` to return null in non-development mode

--- packages/vitnode/src/api/lib/check-plugin-name.ts
+++ packages/vitnode/src/api/lib/check-plugin-name.ts
@@ -17,22 +17,19 @@ export interface PackageJSON {
   workspaces?: string[];
 }
 
-export const checkPluginName = (pluginName: string): PackageJSON => {
+export const checkPluginName = (pluginName: string): null | PackageJSON => {
+  if (!CONFIG.node_development) return null;
+
   const path = join(process.cwd(), 'node_modules', pluginName, 'package.json');
 
-  if (
-    !existsSync(path) &&
-    pluginName !== 'vitnode' &&
-    CONFIG.node_development
-  ) {
+  if (!existsSync(path) && pluginName !== 'vitnode') {
     throw new Error(
       `package.json file not found at ${path}. If you are the developer of this plugin, please ensure you are using the correct plugin name as specified in its package.json.`,
     );
   }
 
   const content: PackageJSON = JSON.parse(readFileSync(path, 'utf-8'));
-
-  if (CONFIG.node_development && content.name !== pluginName) {
+  if (content.name !== pluginName) {
     throw new Error(
       `The plugin name in package.json (${content.name}) does not match the requested plugin name (${pluginName}). Please check the plugin name.`,
     );
