a5c98971edaa1ae889c55cca6be361a80c89d0cc
aXenDeveloper <axendeveloper@gmail.com>
2025-07-02 20:19:28 +0200

    fix(global.d.ts): 🐛 Update `Messages` type to include `blog` localization

--- apps/docs/global.d.ts
+++ apps/docs/global.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="next-intl" />
 
 import type core from './src/locales/@vitnode/core/en.json';
+import type blog from './src/locales/@vitnode/blog/en.json';
 
 declare module 'next-intl' {
   interface AppConfig {
-    Messages: typeof core;
+    Messages: typeof core & typeof blog;
   }
 }
