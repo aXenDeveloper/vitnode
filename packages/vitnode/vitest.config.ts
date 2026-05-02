279873ae47e299b161dca5a7aa7af0892036c68f
aXenDeveloper <axendeveloper@gmail.com>
2026-05-02 18:21:27 +0200

    fix: Run tests via vitest

--- packages/vitnode/vitest.config.ts
+++ packages/vitnode/vitest.config.ts
@@ -1,10 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
