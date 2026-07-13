6732485b0fcf02f1227c6f02a7e919b2d728d251
aXenDeveloper <axendeveloper@gmail.com>
2026-07-13 20:34:33 +0200

    perf: ✨ Update permissions schema to include `unrestricted` and `permissions` fields

--- apps/docs/migrations/0010_open_banshee.sql
+++ apps/docs/migrations/0010_open_banshee.sql
@@ -2,5 +2,7 @@ ALTER TABLE "core_admin_permissions" ADD COLUMN "unrestricted" boolean DEFAULT f
 ALTER TABLE "core_admin_permissions" ADD COLUMN "permissions" jsonb DEFAULT '[]'::jsonb NOT NULL;--> statement-breakpoint
 ALTER TABLE "core_moderators_permissions" ADD COLUMN "unrestricted" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "core_moderators_permissions" ADD COLUMN "permissions" jsonb DEFAULT '[]'::jsonb NOT NULL;--> statement-breakpoint
+UPDATE "core_admin_permissions" SET "unrestricted" = COALESCE(("data"->>'unrestricted')::boolean, false), "permissions" = COALESCE("data"->'permissions', '[]'::jsonb);--> statement-breakpoint
+UPDATE "core_moderators_permissions" SET "unrestricted" = COALESCE(("data"->>'unrestricted')::boolean, false), "permissions" = COALESCE("data"->'permissions', '[]'::jsonb);--> statement-breakpoint
 ALTER TABLE "core_admin_permissions" DROP COLUMN "data";--> statement-breakpoint
 ALTER TABLE "core_moderators_permissions" DROP COLUMN "data";
